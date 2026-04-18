339159bea6313fe940cbc22b2eda372b73157c0b
thippeshr6599 <thippeshr@thestackly.com>
2026-04-18 20:47:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+    var a = 50
+    a = 100
+    console.log(a)
+    
+    let b = 80
+    c = b + 10
+    console.log(c)
+
+    const d = 500
+    const result = d + 100
+    console.log(result)
+
+    let e = "welcome team"
+    console.log(e)
+
+    let f  = "2026"
+    console.log(f)
+
+    let g = "true"
+    console.log(g)
+
+    let name="javaScript"
+    console.log(name)
+
+    let num=250
+    console.log(num)
+
+    let boo = true
+     console.log(boo)
+
+    let fruit = ["apple", "grapes", "mango", "watermelon", "banana"]
+    console.log(fruit)
+    console.log(fruit[0])
+    console.log(fruit[fruit.length-1])
+    
+    let student={
+        name:"Thippesh",
+        age:20,
+        course:"fullstackdeveloper"
+    }
+    console.log(student.name)
+    console.log(student.age)
+
+    let x = 5
+    x++
+    console.log(x)
+
+    let y = 10
+    y--
+    console.log(y)
+
+    let ab=5>2&&10>3
+    console.log(ab)
+
+    let ac=7<5||8>2
+    console.log(ac)
+
+    let ad=!(10>5)
+    console.log(ad)
+
+    let age = 18
+     age>=18 ? console.log("Eligible"):console.log("noteligible")
+
+
